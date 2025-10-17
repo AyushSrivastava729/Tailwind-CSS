2257b18170876ae73249031ea82758c6809eb2d8
AyushSrivastava729 <ayushsrivastava729@gmail.com>
2025-10-18 00:08:00 +0530

    files are being uploaded

---
+++ VIDEO 99/main.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+const Employee = require("./models/Employee")
+
+mongoose.connect('mongodb://127.0.0.1:27017/company');
+const port = 3000
+
+app.set('view engine', 'ejs');
+
+const getRandom = (arr) => {
+  let rno = Math.floor(Math.random() * (arr.length - 1))
+  return arr[rno]
+}
+
+app.get('/', (req, res) => {
+  res.render('index', { foo: 'FOO' });
+})
+
+app.get('/generate', async (req, res) => {
+  // Clear the collection employee
+  await Employee.deleteMany({})
+  // generate random data
+  let randomNames = ["Ayush", "Bhupendra", "Girish", "Saurabh"]
+  let randomLang = ["Python", "JS", "C++", "Java"]
+  let randomCities = ["Bilaspur", "Moradabad", "Mysore", "Kolkata"]
+  for (let index = 0; index < 10; index++) {
+    let e = await Employee.create({
+      name: getRandom(randomNames),
+      salary: Math.floor(Math.random() * 22000),
+      language: getRandom(randomLang),
+      city: getRandom(randomCities),
+      isManager: (Math.random() > 0.5) ? true : false
+    })
+    console.log(e)
+
+
+    // await e.save()
+
+  }
+  res.render('index', { foo: 'FOO' });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
